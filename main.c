af5d8cdc793ae74cfca34467c68c0c3c6207a0e1
lacamillaa <12999356@itisrossi.vi.it>
2025-09-26 22:22:03 +0200

    created backbone for user interaction

--- main.c
+++ main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 
+struct Contatto {
+    char nome[31];
+    char cognome[31];
+    char tel[21];
+};
+
 int main(void) {
-    printf("Hello, World!\n");
+    int scelta = 0;
+    struct Contatto rubrica[100];
+    do {
+        printf("1 - aggiungi un nuovo contatto\n");
+        printf("2 - visualizza tutti i contatti\n");
+        printf("3 - cerca per cognome\n");
+        printf("4 - elimina contatto per cognome\n");
+        printf("0 - esci\n");
+        printf("Inserisci comando: ");
+        scanf("%d", &scelta);
+        switch (scelta) {
+            case 1:
+                // aggiungi contatto
+                break;
+            case 2:
+                // stampa la lista di contatti
+                break;
+            case 3:
+                // cerca
+                break;
+            case 4:
+                // elimina
+                break;
+            case 0:
+                printf("Chiusura del programma in corso...\n");
+                break;
+            default:
+                printf("Inserisci un numero tra 0 e 4\n");
+                break;
+        }
+    } while (scelta != 0);
     return 0;
 }
